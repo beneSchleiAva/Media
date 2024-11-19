b7f7954d9d6593f852afd5172c58eccd9f6f427a
beneSchleiAva <141275829+beneSchleiAva@users.noreply.github.com>
2024-11-19 16:00:18 +0100

    Check for units==0 is implemented by rule

---
+++ Media/Components/Pages/Home.razor.cs
@@ -0,0 +1,34 @@
+﻿using Evaluation.CustomRules;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using ModelInterface.Factories;
+
+namespace Media.Components.Pages
+{
+    public partial class Home
+    {
+        public OrderPositonRule orderPositionRule = new();
+        EventCallback<MouseEventArgs> AssignOrderPositionCallback => new(null, AssignOrderPositionToProduct);
+
+        void AssignOrderPositionToProduct(MouseEventArgs args)
+        {
+            if (selectedProduct is not null && selectedPrice > 0)
+            {
+
+                var productPrice = ProductIdPríce[selectedProduct.Id];
+                if (productPrice > 0)
+                {
+
+                    var position = OrderPositionFactory.Create(productId: selectedProduct.Id, productName: selectedProduct.Name, productPrice: productPrice, quantity: selectedQuantity, totalPrice: selectedPrice);
+                    if (position is not null && orderPositionRule.Evaluate(position).Result)
+                    {
+                        orderPositions.Add(position);
+                        InvokeAsync(StateHasChanged);
+                    }
+                }
+
+            }
+        }
+
+    }
+}
