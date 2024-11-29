2346249909ebb0a1bc400a250fbd6e2e36663a1c
beneSchleiAva <141275829+beneSchleiAva@users.noreply.github.com>
2024-11-29 22:44:03 +0100

    added functionality to create product

--- ReactUI/reactui/src/Components/App/App.tsx
+++ ReactUI/reactui/src/Components/App/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 
 import { ProductGalleryView } from '../Product/ProductView/ProductGallery';
+import ProductCreateForm from '../Product/Create/ProductCreateForm';
+import Grid from '@mui/material/Grid2';
+import { Product } from '../../Types/Product/Product';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props: any) {
@@ -9,9 +13,20 @@ class App extends React.Component {
   }
 
   render() {
-    return      <ProductGalleryView />
+    return <Grid container spacing={2}>
+      <Grid size={8}>
+        <ProductGalleryView />
+      </Grid>
+      <Grid size={4}>
+        <ProductCreateForm CreateFunction={CreateProduct} />
+      </Grid>
+    </Grid>
 
   }
 }
 
+const CreateProduct = (product: Product) => {
+  axios.post('https://localhost:7206/Product', product);
+  console.log(product);
+}
 export default App;
